ff0d215b46add11ab7154196162e897dcb974f52
ch1kulya <164895536+ch1kulya@users.noreply.github.com>
2025-12-26 00:50:25 +0300

    Advanced HTTP logging & better colors

--- logger.go
+++ logger.go
@@ -7,35 +7,40 @@ import (
 )
 
 const (
-	Reset  = "\033[0m"
-	Red    = "\033[31m"
-	Green  = "\033[32m"
-	Yellow = "\033[33m"
-	Blue   = "\033[34m"
-	Cyan   = "\033[36m"
-	Gray   = "\033[37m"
+	Reset = "\033[0m"
+	Bold  = "\033[1m"
+	Dim   = "\033[2m"
+
+	Red     = "\033[91m"
+	Green   = "\033[92m"
+	Yellow  = "\033[93m"
+	Blue    = "\033[94m"
+	Magenta = "\033[95m"
+	Cyan    = "\033[96m"
+	White   = "\033[97m"
+	Gray    = "\033[90m"
 )
 
 func getTimestamp() string {
-	return fmt.Sprintf("%s[%s]%s", Gray, time.Now().Format("15:04:05"), Reset)
+	return fmt.Sprintf("%s%s%s", Dim, time.Now().Format("15:04:05"), Reset)
 }
 
 func Info(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("%s %sINFO%s  %s\n", getTimestamp(), Green, Reset, msg)
+	fmt.Printf("%s %s%-5s%s %s\n", getTimestamp(), Green, "INFO", Reset, msg)
 }
 
 func Error(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Fprintf(os.Stderr, "%s %sERR%s   %s\n", getTimestamp(), Red, Reset, msg)
+	fmt.Fprintf(os.Stderr, "%s %s%-5s%s %s\n", getTimestamp(), Red, "ERR", Reset, msg)
 }
 
 func Warn(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("%s %sWARN%s  %s\n", getTimestamp(), Yellow, Reset, msg)
+	fmt.Printf("%s %s%-5s%s %s\n", getTimestamp(), Yellow, "WARN", Reset, msg)
 }
 
 func Debug(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("%s %sDEBUG%s %s\n", getTimestamp(), Blue, Reset, msg)
+	fmt.Printf("%s %s%-5s%s %s\n", getTimestamp(), Blue, "DEBUG", Reset, msg)
 }
--- middleware.go
+++ middleware.go
@@ -10,6 +10,7 @@ type responseWriterWrapper struct {
 	http.ResponseWriter
 	status      int
 	wroteHeader bool
+	size        int
 }
 
 func (rw *responseWriterWrapper) WriteHeader(code int) {
@@ -19,6 +20,58 @@ func (rw *responseWriterWrapper) WriteHeader(code int) {
 	rw.status = code
 	rw.ResponseWriter.WriteHeader(code)
 	rw.wroteHeader = true
+}
+
+func (rw *responseWriterWrapper) Write(b []byte) (int, error) {
+	if !rw.wroteHeader {
+		rw.WriteHeader(http.StatusOK)
+	}
+	n, err := rw.ResponseWriter.Write(b)
+	rw.size += n
+	return n, err
+}
+
+func getMethodColor(method string) string {
+	switch method {
+	case http.MethodGet:
+		return Green
+	case http.MethodPost:
+		return Cyan
+	case http.MethodPut:
+		return Yellow
+	case http.MethodPatch:
+		return Magenta
+	case http.MethodDelete:
+		return Red
+	case http.MethodOptions, http.MethodHead:
+		return Gray
+	default:
+		return White
+	}
+}
+
+func getStatusColor(status int) string {
+	switch {
+	case status >= 500:
+		return Red
+	case status >= 400:
+		return Yellow
+	case status >= 300:
+		return Cyan
+	default:
+		return Green
+	}
+}
+
+func formatSize(bytes int) string {
+	switch {
+	case bytes >= 1024*1024:
+		return fmt.Sprintf("%.1fM", float64(bytes)/(1024*1024))
+	case bytes >= 1024:
+		return fmt.Sprintf("%.1fK", float64(bytes)/1024)
+	default:
+		return fmt.Sprintf("%dB", bytes)
+	}
 }
 
 func Middleware(next http.Handler) http.Handler {
@@ -32,19 +85,17 @@ func Middleware(next http.Handler) http.Handler {
 
 		next.ServeHTTP(wrapper, r)
 
-		statusColor := Green
-		if wrapper.status >= 400 && wrapper.status < 500 {
-			statusColor = Yellow
-		} else if wrapper.status >= 500 {
-			statusColor = Red
-		}
+		methodColor := getMethodColor(r.Method)
+		statusColor := getStatusColor(wrapper.status)
+		duration := time.Since(start)
 
-		fmt.Printf("%s %s%s%s %s -> %s%d %s%s (%v)\n",
+		fmt.Printf("%s %s%-5s%s %-6s %s%-3d%s %s %s%s%s\n",
 			getTimestamp(),
-			Cyan, r.Method, Reset,
+			methodColor, r.Method, Reset,
+			formatSize(wrapper.size),
+			statusColor, wrapper.status, Reset,
 			r.URL.Path,
-			statusColor, wrapper.status, http.StatusText(wrapper.status), Reset,
-			time.Since(start).Round(time.Millisecond),
+			Dim, duration.Round(time.Microsecond), Reset,
 		)
 	})
 }
